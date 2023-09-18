8bd64bb47abb604dc34f4871eafcb8614ad1fba9
GabrielCarlosLopes <gabriel.lopes@rede.ulbra.br>
2023-09-18 15:06:22 -0300

    add pieces on the correct location and create a onTap function

--- lib/components/square.dart
+++ lib/components/square.dart
@@ -1,13 +1,45 @@
+import 'package:chess_flutter/components/piece.dart';
+import 'package:chess_flutter/values/colors.dart';
 import 'package:flutter/material.dart';
 
 class Square extends StatelessWidget {
   final bool isWhite;
-  const Square({super.key, required this.isWhite});
+  final ChessPiece? piece;
+  final bool isSelected;
+  final void Function()? onTap;
+
+  const Square({
+    super.key,
+    required this.isWhite,
+    required this.piece,
+    required this.isSelected,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: isWhite ? Colors.white : Colors.black,
+    Color? squareColor;
+
+    if (isSelected) {
+      squareColor = Colors.greenAccent;
+    } else {
+      squareColor = isWhite ? foregroundColor : backgroundColor;
+    }
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        color: squareColor,
+        child: piece != null
+            ? Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(
+                  piece!.imagePath,
+                  color: piece!.isWhite ? Colors.white : Colors.black,
+                ),
+              )
+            : null,
+      ),
     );
   }
 }
--- lib/game_board.dart
+++ lib/game_board.dart
@@ -1,5 +1,7 @@
+import 'package:chess_flutter/components/piece.dart';
 import 'package:chess_flutter/components/square.dart';
 import 'package:chess_flutter/helper/helper_methods.dart';
+import 'package:chess_flutter/values/colors.dart';
 import 'package:flutter/material.dart';
 
 class GameBoard extends StatefulWidget {
@@ -10,16 +12,162 @@ class GameBoard extends StatefulWidget {
 }
 
 class _GameBoardState extends State<GameBoard> {
+  late List<List<ChessPiece?>> board;
+
+  ChessPiece? selectedPiece;
+
+  int selectedRow = -1;
+
+  int selectedCol = -1;
+
+  void _initalizeBoard() {
+    late List<List<ChessPiece?>> newBoard =
+        List.generate(8, (index) => List.generate(8, (index) => null));
+
+    // Place pawns
+    for (var i = 0; i < 8; i++) {
+      newBoard[1][i] = ChessPiece(
+        type: ChessPieceType.pawn,
+        isWhite: false,
+        imagePath: 'assets/pieces/pawn.png',
+      );
+      newBoard[6][i] = ChessPiece(
+        type: ChessPieceType.pawn,
+        isWhite: true,
+        imagePath: 'assets/pieces/pawn.png',
+      );
+    }
+
+    // Place rooks
+    newBoard[0][0] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: false,
+      imagePath: 'assets/pieces/rook.png',
+    );
+    newBoard[0][7] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: false,
+      imagePath: 'assets/pieces/rook.png',
+    );
+    newBoard[7][0] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: true,
+      imagePath: 'assets/pieces/rook.png',
+    );
+    newBoard[7][7] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: true,
+      imagePath: 'assets/pieces/rook.png',
+    );
+
+    // Place knights
+    newBoard[0][1] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: false,
+      imagePath: 'assets/pieces/knight.png',
+    );
+    newBoard[0][6] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: false,
+      imagePath: 'assets/pieces/knight.png',
+    );
+    newBoard[7][1] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: true,
+      imagePath: 'assets/pieces/knight.png',
+    );
+    newBoard[7][6] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: true,
+      imagePath: 'assets/pieces/knight.png',
+    );
+
+    // Place bishops
+    newBoard[0][2] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: false,
+      imagePath: 'assets/pieces/bishop.png',
+    );
+    newBoard[0][5] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: false,
+      imagePath: 'assets/pieces/bishop.png',
+    );
+    newBoard[7][2] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: true,
+      imagePath: 'assets/pieces/bishop.png',
+    );
+    newBoard[7][5] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: true,
+      imagePath: 'assets/pieces/bishop.png',
+    );
+
+    // Place queens
+    newBoard[0][3] = ChessPiece(
+      type: ChessPieceType.queen,
+      isWhite: false,
+      imagePath: 'assets/pieces/queen.png',
+    );
+    newBoard[7][4] = ChessPiece(
+      type: ChessPieceType.queen,
+      isWhite: true,
+      imagePath: 'assets/pieces/queen.png',
+    );
+
+    // Place kings
+    newBoard[0][4] = ChessPiece(
+      type: ChessPieceType.king,
+      isWhite: false,
+      imagePath: 'assets/pieces/king.png',
+    );
+    newBoard[7][3] = ChessPiece(
+      type: ChessPieceType.king,
+      isWhite: true,
+      imagePath: 'assets/pieces/king.png',
+    );
+
+    board = newBoard;
+  }
+
+  void pieceSelected(int row, int col) {
+    setState(() {
+      if (board[row][col] != null) {
+        selectedPiece = board[row][col];
+        selectedCol = col;
+        selectedRow = row;
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initalizeBoard();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: backgroundColor,
       body: GridView.builder(
         physics: const NeverScrollableScrollPhysics(),
         itemCount: 8 * 8,
         gridDelegate:
             const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),
         itemBuilder: (context, index) {
-          return Square(isWhite: isWhite(index));
+          int row = index ~/ 8;
+          int col = index % 8;
+
+          bool isSelected = selectedCol == col && selectedRow == row;
+
+          return Square(
+            isWhite: isWhite(index),
+            piece: board[row][col],
+            isSelected: isSelected,
+            onTap: () => pieceSelected(row, col),
+          );
         },
       ),
     );
